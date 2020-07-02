617610e433effe5c65782bb1cd783fd47234b023
Raman Gupta <7243222+raman325@users.noreply.github.com>
2020-07-01 23:47:19 -0400

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/raman325/pyclimacell",
-    version="0.10.0",
+    version="0.11.0",
     zip_safe=False,
 )
