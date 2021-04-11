423d794b43312a9f10d96e0bb384b02b99e2f7f9
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-04-11 01:41:01 -0400

    Bump version: 0.18.0 → 0.18.1

--- pyclimacell/__init__.py
+++ pyclimacell/__init__.py
@@ -4,4 +4,4 @@ from .pyclimacell import ClimaCellV3, ClimaCellV4, ClimaCellV3Sync, ClimaCellV4S
 
 __author__ = """raman325"""
 __email__ = "7243222+raman325@users.noreply.github.com"
-__version__ = "0.18.0"
+__version__ = "0.18.1"
